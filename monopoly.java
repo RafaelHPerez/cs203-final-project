829136a10f569d0e683c2170495493769cf36c0b
RafaelHPerez <143423289+RafaelHPerez@users.noreply.github.com>
2023-10-18 14:50:43 +0000

    added base for methods.

--- monopoly.java
+++ monopoly.java
@@ -1,4 +1,16 @@
 import java.util.Random;
+import java.util.ArrayList;
+
+
+
+
+//add to method to to roll the dice to move on the board
 int rand_int1 = rand.nextInt(6);
 int rand_int2 = rand.nextInt(6);
 int d = rand_int1 + rand_int2;
+//create property object to make an array list from
+ArrayList<>  = new ArrayList<>();
+//treasure chest. 
+ArrayList<>  = new ArrayList<>();
+//community chest
+ArrayList<>  = new ArrayList<>();
