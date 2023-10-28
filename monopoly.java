150102ebe3a3412a277d718333ef9d651b38384f
RafaelHPerez <143423289+RafaelHPerez@users.noreply.github.com>
2023-10-28 14:34:07 +0000

    fixed some problems

--- monopoly.java
+++ monopoly.java
@@ -1,6 +1,7 @@
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.*;
+public class mainn{
 public static void main(String args) {
 //add to method to to roll the dice to move on the board
 int rand_int1 = rand.nextInt(6);
@@ -8,19 +9,19 @@ int rand_int2 = rand.nextInt(6);
 int d = rand_int1 + rand_int2;
 //create property object to make an array list from
 //find type for array list bellow
-ArrayList<> intger = new ArrayList<>();
+ArrayList<> integer1 = new ArrayList<>();
 //treasure chest. 
 //find type for array list bellow
-ArrayList<> integer = new ArrayList<>();
+ArrayList<> integer2 = new ArrayList<>();
 //community chest
 //find type for array list bellow
-ArrayList<> integer = new ArrayList<>();
+ArrayList<> integer3= new ArrayList<>();
 }
 
 
 
 
-//this may not be right but, its a start 
+//this may not be right but, its a start. this method rolls the dice and moves u forward.
 public class roll {
     static void dice() {
         Scanner imput = new Scanner(System.in);  // Create a Scanner object
@@ -34,12 +35,31 @@ System.out.println(d );
 else{system.out.println("invalid imput");}
 //add a while loop that keeps track of how many doubles each player has gotten per turn.  
 
-
-
 if(rand_int1!=rand_int2){}//figure out how to move players postiotion, end turn and move to the next player. 
 
 
 
+}
+//this method draws a chance card when you land on the chance space.
+public class chance {
+    static void chancecard () {
+        //add  once we add postion and board spaces and chance cards. 
+        
+system.out.println();//print the chance card that was pulled. 
+//do what the chance card says
+}
 
-    }
-    
+//this method draws a tresure card when you land on the tresure space.
+public class Community {
+    static void Communitychest () {
+        //add  once we add postion and board spaces and the Community chest. 
+        
+system.out.println();//print the Community chest card  that was pulled. 
+//do what the Community chest card says.
+}
+
+
+}
+}
+}
+}
