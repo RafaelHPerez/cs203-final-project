430ddfa97fde07f87526d2f1aa5adaaac7579bba
RafaelHPerez <143423289+RafaelHPerez@users.noreply.github.com>
2023-10-28 13:44:45 +0000

    started the roll method. still an error on line 4

--- monopoly.java
+++ monopoly.java
@@ -7,8 +7,39 @@ int rand_int1 = rand.nextInt(6);
 int rand_int2 = rand.nextInt(6);
 int d = rand_int1 + rand_int2;
 //create property object to make an array list from
-ArrayList<>  = new ArrayList<>();
+//find type for array list bellow
+ArrayList<> intger = new ArrayList<>();
 //treasure chest. 
-ArrayList<>  = new ArrayList<>();
+//find type for array list bellow
+ArrayList<> integer = new ArrayList<>();
 //community chest
-ArrayList<>  = new ArrayList<>();
+//find type for array list bellow
+ArrayList<> integer = new ArrayList<>();
+}
+
+
+
+
+//this may not be right but, its a start 
+public class roll {
+    static void dice() {
+        Scanner imput = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Type roll to roll the dice");
+if(imput== "roll"){int rand_int1 = rand.nextInt(6);
+    int rand_int2 = rand.nextInt(6);
+    int d = rand_int1 + rand_int2;
+System.out.println(d );
+//figure out how to move players postiotion 
+}
+else{system.out.println("invalid imput");}
+//add a while loop that keeps track of how many doubles each player has gotten per turn.  
+
+
+
+if(rand_int1!=rand_int2){}//figure out how to move players postiotion, end turn and move to the next player. 
+
+
+
+
+    }
+    
