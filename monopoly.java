92a3ff6383dfbc87647a05c9676cb6d28038fee7
RafaelHPerez <143423289+RafaelHPerez@users.noreply.github.com>
2023-11-06 21:35:45 +0000

    fix roll

--- monopoly.java
+++ monopoly.java
@@ -1,11 +1,10 @@
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.*;
-public class mainn{
+public class monopoly{
 public static void main(String args) {
 //add to method to to roll the dice to move on the board
-int rand_int1 = rand.nextInt(6);
-int rand_int2 = rand.nextInt(6);
+
 int d = rand_int1 + rand_int2;
 //create property object to make an array list from
 //find type for array list bellow
@@ -18,24 +17,30 @@ ArrayList<> integer2 = new ArrayList<>();
 ArrayList<> integer3= new ArrayList<>();
 }
 
-
-
-
-//this may not be right but, its a start. this method rolls the dice and moves u forward.
-public class roll {
-    static void dice() {
-        Scanner imput = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Type roll to roll the dice");
-if(imput== "roll"){int rand_int1 = rand.nextInt(6);
+//roll method
+public int rollofdice ()
+{
+       
+    return //dice roll
+    int rand_int1 = rand.nextInt(6);
     int rand_int2 = rand.nextInt(6);
-    int d = rand_int1 + rand_int2;
-System.out.println(d );
+    int roll = rand_int1 + rand_int2;
+    return roll;
+}
+
+   
 //figure out how to move players postiotion 
 }
 else{system.out.println("invalid imput");}
 //add a while loop that keeps track of how many doubles each player has gotten per turn.  
 
 if(rand_int1!=rand_int2){}//figure out how to move players postiotion, end turn and move to the next player. 
+}
+
+
+//this may not be right but, its a start. this method rolls the dice and moves u forward.
+
+   
 
 
 
