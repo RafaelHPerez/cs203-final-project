ee5c0e0696709e690bd7394822a0023c503465d2
RafaelHPerez <143423289+RafaelHPerez@users.noreply.github.com>
2023-11-06 22:21:42 +0000

    added stuff

--- monopoly.java
+++ monopoly.java
@@ -1,70 +1,107 @@
-import java.util.Random;
-import java.util.ArrayList;
 import java.util.*;
 public class monopoly{
-public static void main(String args) {
+public static void main(String[] args) {
 //add to method to to roll the dice to move on the board
-
-int d = rand_int1 + rand_int2;
+System.out.println(rollofdice());
 //create property object to make an array list from
 //find type for array list bellow
-ArrayList<> integer1 = new ArrayList<>();
-//treasure chest. 
-//find type for array list bellow
-ArrayList<> integer2 = new ArrayList<>();
-//community chest
-//find type for array list bellow
-ArrayList<> integer3= new ArrayList<>();
-}
+// ArrayList<> integer1 = new ArrayList<>();
+// //treasure chest. 
+// //find type for array list bellow
+// ArrayList<> integer2 = new ArrayList<>();
+// //community chest
+// //find type for array list bellow
+// ArrayList<> integer3= new ArrayList<>();
+
+}// end main
 
 //roll method
-public int rollofdice ()
+public static int rollofdice ()
 {
-       
-    return //dice roll
-    int rand_int1 = rand.nextInt(6);
-    int rand_int2 = rand.nextInt(6);
-    int roll = rand_int1 + rand_int2;
+    Random rand = new Random();
+    int dice1 = rand.nextInt(6) + 1;
+    int dice2 = rand.nextInt(6) + 1;
+    int roll = dice1 + dice2;
     return roll;
 }
 
-   
-//figure out how to move players postiotion 
+public static ArrayList<String> createBoard()
+{//ADD names of board spaces
+    ArrayList<String> board = new ArrayList<String>();
+    board.add("GO");
+    board.add("Mediterranean Avenue");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    board.add("GO");
+    return board;
+    
 }
-else{system.out.println("invalid imput");}
+
+}//end class
+
+//figure out how to move players postiotion 
+
+
 //add a while loop that keeps track of how many doubles each player has gotten per turn.  
 
-if(rand_int1!=rand_int2){}//figure out how to move players postiotion, end turn and move to the next player. 
-}
 
 
 //this may not be right but, its a start. this method rolls the dice and moves u forward.
 
    
 
-
-
-}
 //this method draws a chance card when you land on the chance space.
-public class chance {
-    static void chancecard () {
-        //add  once we add postion and board spaces and chance cards. 
+// public class chance {
+//     static void chancecard () {
+//         //add  once we add postion and board spaces and chance cards. 
         
-system.out.println();//print the chance card that was pulled. 
-//do what the chance card says
-}
+// system.out.println();//print the chance card that was pulled. 
+// //do what the chance card says
+// }
 
-//this method draws a tresure card when you land on the tresure space.
-public class Community {
-    static void Communitychest () {
-        //add  once we add postion and board spaces and the Community chest. 
+// //this method draws a tresure card when you land on the tresure space.
+// public class Community {
+//     static void Communitychest () {
+//         //add  once we add postion and board spaces and the Community chest. 
         
-system.out.println();//print the Community chest card  that was pulled. 
-//do what the Community chest card says.
-}
+// system.out.println();//print the Community chest card  that was pulled. 
+// //do what the Community chest card says.
+// }
 
 
-}
-}
-}
-}
+//}
